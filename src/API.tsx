654055ef937566fa09956401e8be0b2462cb726c
Yohai Ararat <hello@ararat.works>
2022-10-26 18:06:13 -0400

    updated the http to https

--- src/API.tsx
+++ src/API.tsx
@@ -6,7 +6,7 @@ export const geoApiOptions = {
   },
 };
 // export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-export const GEO_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}";
+export const GEO_API_URL = "https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "678a374eefbdfaf6c0ebe139725629f2"; // enter your key from openweather API
